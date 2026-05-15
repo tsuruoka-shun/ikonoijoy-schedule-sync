4c175dd9596f68dd76a8adffe00560ecb037d369
tsuruoka-shun <280244985+tsuruoka-shun@users.noreply.github.com>
2026-05-15 18:04:55 +0900

    feature: Googleカレンダーに予定追加する機能の実装

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+
+from schedule_calendar.add_schedules import add_schedules
 from scraper.get_schedules import get_schedules
 
 logging.basicConfig(level=logging.INFO)
@@ -14,6 +16,12 @@ def main():
     logger.info("not_equal_me: %d件", len(schedules["not_equal_me"]))
     logger.info("nearly_equal_joy: %d件", len(schedules["nearly_equal_joy"]))
     logger.info("scraping done")
+
+    logger.info("add to schedules start")
+
+    add_schedules(schedules)
+
+    logger.info("add to schedules done")
 
 
 if __name__ == "__main__":
