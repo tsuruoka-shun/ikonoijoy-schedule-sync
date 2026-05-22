aa83f601cafe072765dffa6b0086da84c035543c
tsuruoka-shun <280244985+tsuruoka-shun@users.noreply.github.com>
2026-05-22 17:02:27 +0900

    refactor: ディレクトリ名、ファイル名変更

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 
-from schedule_calendar.sync_schedules import sync_schedules
-from scraper.get_schedules import get_schedules
+from sync.google_calendar import sync_schedules
+from scraper.schedule import get_schedules
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
